3dcb0e63482858f591637d1228662c6892d02dd3
Clayton Santos <claytondesousasantos@gmail.com>
2021-10-23 12:41:14 -0300

    Primeiro commit

---
+++ frontend/src/app/components/products/products-create/products-create.component.ts
@@ -0,0 +1,34 @@
+import { Product } from './../product.model';
+import { ProductService } from './../../product/product.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-products-create',
+  templateUrl: './products-create.component.html',
+  styleUrls: ['./products-create.component.css']
+})
+export class ProductsCreateComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: null
+
+  }
+
+  constructor(private productService: ProductService, private router: Router) { }
+
+  ngOnInit(): void {
+    
+  }
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado!')
+      this.router.navigate(['/products'])
+    })
+    
+  }
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+}
